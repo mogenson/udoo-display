f3256e83765a5d38b0ca95d2294cbf595d31d14b
Michael Mogenson <michael.mogenson@gmail.com>
2020-09-25 13:59:56 -0400

    Use cc crate to build and link usb_serial C code as static library

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() -> ! {
     let dp = hal::Peripherals::take().unwrap();
     let mut pins = hal::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF);
     let mut led = pins.d13.into_output(&mut pins.ddr);
+
     let i2c = hal::I2c::new(
         dp.TWI,
         pins.d2.into_pull_up_input(&mut pins.ddr),
@@ -26,16 +27,33 @@ fn main() -> ! {
     let mut disp: TerminalMode<_> = Builder::new().connect(interface).into();
     disp.init().unwrap();
     disp.clear().unwrap();
-    disp.write_str("USB init\n").unwrap();
+    disp.write_str("init ").unwrap();
 
-    usb_serial::usb_init(dp.USB_DEVICE, &dp.PLL);
-    while !usb_serial::get_cfg_status() {
-        hal::delay_ms(5);
-    }
-    disp.write_str("done").unwrap();
+    usb_serial::init();
+    while !usb_serial::is_configured() {}
+    avr_device::interrupt::free(|_| disp.write_str("done ").ok());
+    while !usb_serial::get_dtr() {}
+    usb_serial::flush_input();
+    avr_device::interrupt::free(|_| disp.write_str("open\n").ok());
 
     loop {
-        hal::delay_ms(1000);
+        while usb_serial::get_available() > 0 {
+            if let Some(c) = usb_serial::get_char() {
+                avr_device::interrupt::free(|_| disp.print_char(c as char).ok());
+            }
+        }
+
+        hal::delay_ms(200);
         led.toggle().void_unwrap();
     }
+}
+
+#[avr_device::interrupt(atmega32u4)]
+unsafe fn USB_GEN() {
+    avr_device::interrupt::free(|_| usb_serial::isr_usb_gen_vect());
+}
+
+#[avr_device::interrupt(atmega32u4)]
+unsafe fn USB_COM() {
+    avr_device::interrupt::free(|_| usb_serial::isr_usb_com_vect());
 }
--- src/usb_serial.rs
+++ src/usb_serial.rs
@@ -1,159 +1,63 @@
-use arduino_leonardo as hal;
-use avr_device::interrupt; // clear flags to handshake
-use core::sync::atomic::{AtomicBool, AtomicU8, Ordering::SeqCst};
-use hal::atmega32u4::USB_DEVICE;
+#![allow(dead_code)]
 
-static mut USB: Option<USB_DEVICE> = None;
-static mut USB_CONFIGURATION: AtomicU8 = AtomicU8::new(0); // non-zero when enumerated
+extern "C" {
+    /* setup */
+    fn usb_init();
+    fn usb_configured() -> u8;
 
-// static mut CDC_LINE_CODING: [u8; 7] = [0, 255, 0, 0, 0, 0, 8];
-// static mut CDC_LINE_RTSDTR: AtomicU8 = AtomicU8::new(0);
+    /* receiving data */
+    fn usb_serial_getchar() -> i16;
+    fn usb_serial_available() -> u8;
+    fn usb_serial_flush_input();
 
-static mut CONFIG_STATUS: AtomicBool = AtomicBool::new(false);
+    /* transmitting data */
+    fn usb_serial_putchar(c: u8) -> i8;
+    fn usb_serial_putchar_nowait(c: u8) -> i8;
+    fn usb_serial_write(buffer: *const u8, size: u16) -> i8;
+    fn usb_serial_flush_output();
 
-static ENDPOINT_CONFIG_TABLE: [u8; 10] =
-    [0x00, 0x01, 0xC1, 0x12, 0x01, 0x80, 0x36, 0x01, 0x81, 0x36];
+    /* serial parameters */
+    fn usb_serial_get_baud() -> u32;
+    fn usb_serial_get_stopbits() -> u8;
+    fn usb_serial_get_paritytype() -> u8;
+    fn usb_serial_get_numbits() -> u8;
+    fn usb_serial_get_control() -> u8;
+    fn usb_serial_set_control(signals: u8) -> i8;
 
-pub fn get_cfg_status() -> bool {
-    unsafe { CONFIG_STATUS.load(SeqCst) }
+    /* interrupt service routines */
+    pub fn isr_usb_gen_vect();
+    pub fn isr_usb_com_vect();
 }
 
-unsafe fn usb_ack_out(usb: &USB_DEVICE) {
-    usb.ueintx.write(|w| w.bits(0xFF).rxouti().clear_bit());
+pub fn init() {
+    unsafe { usb_init() };
 }
 
-unsafe fn usb_send_in(usb: &USB_DEVICE) {
-    usb.ueintx.write(|w| w.bits(0xFF).txini().clear_bit());
+pub fn is_configured() -> bool {
+    unsafe { usb_configured() != 0 }
 }
 
-unsafe fn usb_wait_in_ready(usb: &USB_DEVICE) {
-    while usb.ueintx.read().txini().bit_is_clear() {}
-}
-
-unsafe fn usb_wait_receive_out(usb: &USB_DEVICE) {
-    while usb.ueintx.read().rxouti().bit_is_clear() {}
-}
-
-#[avr_device::interrupt(atmega32u4)]
-unsafe fn USB_GEN() {
-    let usb = get_usb();
-    // end of reset interrupt
-    if usb.udint.read().eorsti().bit_is_set() {
-        usb.udint.write(|w| w.eorsti().clear_bit()); // clear interrupt flag
-        usb.uenum.write(|w| w.bits(0)); // select ep 0
-        usb.ueconx.write(|w| w.epen().set_bit()); // enable selected ep
-        usb.uecfg0x
-            .write(|w| w.eptype().bits(0).epdir().clear_bit()); // set ep to control type an out direction
-                                                                // set ep size to 16 bytes, use single bank, and allocate ep
-        usb.uecfg1x
-            .write(|w| w.epsize().bits(1).epbk().bits(0).alloc().set_bit()); // set ep size to 16 bytes
-
-        usb.ueienx.write(|w| w.rxstpe().set_bit()); // interrupt on setup packet
-        CONFIG_STATUS.store(usb.uesta0x.read().cfgok().bit_is_set(), SeqCst);
+pub fn get_char() -> Option<u8> {
+    let c = unsafe { usb_serial_getchar() };
+    if c == -1 {
+        None
+    } else {
+        Some(c as u8)
     }
 }
 
-#[avr_device::interrupt(atmega32u4)]
-unsafe fn USB_COM() {
-    let usb = get_usb();
-    usb.uenum.write(|w| w.bits(0)); // select ep 0
-
-    if usb.ueintx.read().rxstpi().bit_is_clear() {
-        usb.ueconx.write(|w| w.stallrq().set_bit().epen().set_bit()); // stall
-        return;
-    }
-
-    let bm_request_type = usb.uedatx.read().bits();
-    let b_request = usb.uedatx.read().bits();
-    let mut w_value = usb.uedatx.read().bits() as u16;
-    w_value |= (usb.uedatx.read().bits() as u16) << 8;
-    let mut w_index = usb.uedatx.read().bits() as u16;
-    w_index |= (usb.uedatx.read().bits() as u16) << 8;
-    let mut w_length = usb.uedatx.read().bits() as u16;
-    w_length |= (usb.uedatx.read().bits() as u16) << 8;
-    usb.ueintx.write(|w| {
-        w.bits(0xFF)
-            .rxstpi()
-            .clear_bit()
-            .rxouti()
-            .clear_bit()
-            .txini()
-            .clear_bit()
-    }); // clear flags to handshake
-
-    match b_request {
-        0x00 => {
-            // get status
-            usb_wait_in_ready(&usb);
-            usb.uedatx.write(|w| w.bits(0));
-            usb.uedatx.write(|w| w.bits(0));
-            usb_send_in(&usb);
-        }
-        0x05 => {
-            // set address
-            usb_send_in(&usb);
-            usb_wait_in_ready(&usb);
-            usb.udaddr
-                .write(|w| w.bits(w_value as u8).adden().set_bit());
-        }
-        0x06 => (), // get descriptor
-        0x08 => {
-            if bm_request_type == 0x80 {
-                // get configuration
-                usb_wait_in_ready(&usb);
-                usb.uedatx.write(|w| w.bits(USB_CONFIGURATION.load(SeqCst)));
-                usb_send_in(&usb);
-            }
-        }
-        0x09 => {
-            if bm_request_type == 0x00 {
-                // set configuration
-                USB_CONFIGURATION.store(w_value as u8, SeqCst);
-                // cdc_line_rtsdtr = 0
-                // transmit_flush_timer = 0
-                usb_send_in(&usb);
-                let mut cfg = ENDPOINT_CONFIG_TABLE.iter();
-                for i in 1..5 {
-                    usb.uenum.write(|w| w.bits(i));
-                    let en = *cfg.next().unwrap();
-                    usb.ueconx.write(|w| w.bits(en));
-                    if en != 0 {
-                        usb.uecfg0x.write(|w| w.bits(*cfg.next().unwrap()));
-                        usb.uecfg1x.write(|w| w.bits(*cfg.next().unwrap()));
-                    }
-                }
-                usb.uerst.write(|w| w.bits(0x1E));
-                usb.uerst.write(|w| w.bits(0));
-            }
-        }
-        0x20 => if bm_request_type == 0x21 {}, // set cdc line coding
-        0x21 => if bm_request_type == 0xA1 {}, // get cdc line coding
-        0x22 => if bm_request_type == 0x21 {}, // set cdc control line state
-        _ => (),
-    };
+pub fn get_available() -> u8 {
+    unsafe { usb_serial_available() }
 }
 
-unsafe fn get_usb() -> &'static USB_DEVICE {
-    USB.as_ref().unwrap()
+pub fn flush_input() {
+    unsafe { usb_serial_flush_input() }
 }
 
-pub fn usb_init(usb: USB_DEVICE, pll: &hal::atmega32u4::PLL) {
-    interrupt::free(|_| {
-        usb.uhwcon.write(|w| w.uvrege().set_bit()); // enable pad regulator
-        usb.usbcon.write(|w| w.usbe().set_bit().frzclk().set_bit()); // freeze usb clock
-        pll.pllcsr.write(|w| w.pindiv().set_bit().plle().set_bit()); // enable PLL for 16 mhz clock
-        while pll.pllcsr.read().plock().bit_is_clear() {} // wait for PLL lock
-        usb.usbcon
-            .write(|w| w.usbe().set_bit().frzclk().clear_bit().otgpade().set_bit()); // enable usb and vusb
-        usb.udcon.write(|w| w.detach().clear_bit()); // enable pull up resistors
-        usb.udien.write(|w| w.eorste().set_bit().sofe().set_bit()); // enable end of reset interrupts
+pub fn get_dtr() -> bool {
+    unsafe { usb_serial_get_control() & 0x01 != 0 }
+}
 
-        unsafe {
-            USB = Some(usb); // save peripheral in global
-        }
-    });
-
-    unsafe {
-        interrupt::enable();
-    }
+pub fn get_rts() -> bool {
+    unsafe { usb_serial_get_control() & 0x02 != 0 }
 }
